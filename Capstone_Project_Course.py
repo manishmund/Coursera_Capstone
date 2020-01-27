14f5a53b8087ee7c73470679edc4ad2e472b4080
manishmund <manishmund19@gmail.com>
2020-01-28 05:09:18 +0530

    Add files via upload

---
+++ Capstone_Project_Course.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is meant for Coursera Capstone course on Applied data science by IBM 
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
